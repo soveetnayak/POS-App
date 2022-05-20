b631d001242b49aeee6f45e28d87fb5b63b98005
Ayush Agrawal <ayush.agriiit@gmail.com>
2022-05-20 21:39:37 +0530

    Added inventory search bar

--- main/src/components/Inventory/Inventory.js
+++ main/src/components/Inventory/Inventory.js
@@ -9,55 +9,98 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Container, TextField } from "@mui/material";
+import { Box } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import Fuse from "fuse.js";
 
 function Inventory() {
+
+    const [searchText, setSearchText] = useState('');
     const [inventory, setInventory] = useState([]);
+    const [searchResult, setSearchResult] = useState([]);
     const collectionRef = collection(db, "Inventory");
 
     useEffect(() => {
         const getItems = async () => {
             const data = await getDocs(collectionRef);
-            console.log(data);
             setInventory(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-            console.log(inventory);
+            setSearchResult(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
         }
-        
+
         getItems();
     }, []);
+
+    const handleSearchTextChange = (event) => {
+
+        setSearchText(event.target.value);
+        const fuse = new Fuse(inventory, {
+            keys: ["name", "description", "category", "tag"]
+        })
+        if(event.target.value != '')
+        {
+            const filteredData = fuse.search(event.target.value);
+            const temp = [];
+            filteredData.map(res => {
+                temp.push(res.item);
+            })
+            setSearchResult([...temp]);
+        }
+        else
+        {
+            setSearchResult(inventory);
+        }
+
+    }
 
     //return a table with the inventory
     return (
         <div>
-            <TableContainer component={Paper}>
-            <Table aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Name</TableCell>
-                        <TableCell >Price</TableCell>
-                        <TableCell >Quantity</TableCell>
-                        <TableCell >Description</TableCell>
-                        <TableCell >Category</TableCell>
-                        <TableCell >Tags</TableCell>
-                        <TableCell >Image</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {inventory.map((item) => (
-                        <TableRow key={item.id} >
-                            <TableCell>{item.name}</TableCell>
-                            <TableCell>{item.price}</TableCell>
-                            <TableCell>{item.quantity}</TableCell>
-                            <TableCell>{item.description}</TableCell>
-                            <TableCell>{item.category.join(', ')}</TableCell>
-                            <TableCell>{item.tag.join(', ')}</TableCell>
-                            <TableCell>
-                                <img src={item.image} alt="product" width="100" height="100" />
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-            </TableContainer>
+            <Container maxWidth="xl">
+                <Box
+                    display="flex"
+                    alignItems="center"
+                >
+                    <TextField
+                        fullWidth
+                        label="Search Inventory"
+                        value={searchText}
+                        onChange={handleSearchTextChange}
+                    >
+                    </TextField>
+                    <SearchIcon />
+                </Box>
+                <TableContainer component={Paper}>
+                    <Table aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell >Price</TableCell>
+                                <TableCell >Quantity</TableCell>
+                                <TableCell >Description</TableCell>
+                                <TableCell >Category</TableCell>
+                                <TableCell >Tags</TableCell>
+                                <TableCell >Image</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {searchResult.map((item) => (
+                                <TableRow key={item.id} >
+                                    <TableCell>{item.name}</TableCell>
+                                    <TableCell>{item.price}</TableCell>
+                                    <TableCell>{item.quantity}</TableCell>
+                                    <TableCell>{item.description}</TableCell>
+                                    <TableCell>{item.category.join(', ')}</TableCell>
+                                    <TableCell>{item.tag.join(', ')}</TableCell>
+                                    <TableCell>
+                                        <img src={item.image} alt="product" width="100" height="100" />
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Container>
         </div>
     );
 
