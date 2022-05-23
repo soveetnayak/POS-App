011db64ac51adc1463ff2bb2db416c66431b64e2
Ayush Agrawal <ayush.agriiit@gmail.com>
2022-05-23 18:49:52 +0530

    Added billing features

--- main/src/components/Billing/Billing.js
+++ main/src/components/Billing/Billing.js
@@ -1,8 +1,9 @@
 import './Billing.css';
 import { useState, useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { addDoc, collection, getDocs } from "firebase/firestore";
 import { db } from "../../FirebaseConfig";
 
+import { Grid, Typography, Container, TextField, Autocomplete, Dialog, IconButton, RadioGroup, Radio } from '@mui/material';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -10,104 +11,452 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { DataGrid } from '@mui/x-data-grid';
 import { Button } from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Box } from '@mui/system';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
-function Billing () {
+const emptyProduct = {
+  id: "",
+  name: "",
+  description: "",
+  category: [],
+  tag: [],
+  image: "",
+}
+
+function Billing() {
+
+
+  const [custName, setCustName] = useState('');
+  const [curProduct, setCurProduct] = useState(emptyProduct);
+  const [curProductInputValue, setCurProductInputValue] = useState('');
+
+  const [curQuantity, setCurQuantity] = useState('');
+  const [curQuantityIsValid, setCurQuantityIsValid] = useState(false);
+  const [curQuantityErrMessage, setCurQuantityErrMessage] = useState("");
+  const [curQuantityIsTouched, setCurQuantityIsTouched] = useState(false);
+
+  const [totalamount, setTotalAmount] = useState('');
   const [inventory, setInventory] = useState([]);
   const [cart, setCart] = useState([]);
-    const collectionRef = collection(db, "Inventory");
+  const [billDialog, setBillDialog] = useState(false);
+  const collectionRefInventory = collection(db, "Inventory");
+  const collectionRefBills = collection(db, "Bills");
 
-    useEffect(() => {
-      const getItems = async () => {
-          const data = await getDocs(collectionRef);
-          setInventory(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-      }
+  const date = new Date();
+  const [curTime, setCurTime] = useState(date.toLocaleTimeString().slice(0, -3));
 
-      getItems();
+  useEffect(() => {
+    const getItems = async () => {
+      const data = await getDocs(collectionRefInventory);
+      setInventory(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    }
+
+    getItems();
   }, []);
 
-  const handleSelect = (row) => {
-    const temp = [...cart];
-    temp.push(row.id);
-    setCart(temp);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      var temp = new Date();
+      setCurTime(temp.toLocaleTimeString().slice(0, -3));
+    }, 60000);
+    return () => {
+      clearInterval(timer);
+    }
+  }, [])
+
+  const onChangeCustName = (event) => {
+    setCustName(event.target.value);
+  }
+
+  const handleCurProductChange = (event, newValue) => {
+    console.log(newValue);
+    if (newValue != null)
+      setCurProduct(newValue);
+    else
+      setCurProduct(emptyProduct)
+  }
+
+  const handleCurProductInputValueChange = (event, newInputValue) => {
+    if (newInputValue != '')
+      setCurProductInputValue(newInputValue);
+  }
+
+  const checkCurQuantityIsValid = (qty) => {
+
+    if (isNaN(Number(qty))) {
+      setCurQuantityIsValid(false);
+      setCurQuantityErrMessage("Quantity must be a valid number");
+      return;
+    }
+
+    if (Number(qty) <= 0 || !Number.isInteger(Number(qty))) {
+      setCurQuantityIsValid(false);
+      setCurQuantityErrMessage("Quantity must be a positive whole number");
+      return;
+    }
+
+    if (Number(qty) > +curProduct.quantity) {
+      setCurQuantityIsValid(false);
+      setCurQuantityErrMessage("Quantity must be less than available quantity");
+      return;
+    }
+
+    if (!curQuantityIsValid)
+      setCurQuantityIsValid(true);
+
+    return;
+  }
+
+  const onChangeCurQuantity = (event) => {
+    if (!curQuantityIsTouched)
+      setCurQuantityIsTouched(true);
+    setCurQuantity(event.target.value);
+    checkCurQuantityIsValid(event.target.value);
+  }
+
+  const onAddToCart = () => {
+    if (!curQuantityIsValid) {
+      alert(curQuantityErrMessage);
+      return;
+    }
+    const temp = cart;
+    temp.push({
+      id: curProduct.id,
+      image: curProduct.image,
+      name: curProduct.name,
+      description: curProduct.description,
+      price: curProduct.price,
+      quantity: curQuantity,
+    });
+    const newTotal = +totalamount + +curProduct.price * +curQuantity;
+    setTotalAmount(newTotal);
+    setCart([...temp]);
+    setCurProduct(emptyProduct);
+    setCurQuantity('');
+  }
+
+  const onDeleteItem = (id) => {
+    console.log(id);
+    const temp = cart;
+    const deleteidx = temp.findIndex(item => item.id === id);
+    console.log(deleteidx);
+    const newTotal = +totalamount - (temp[deleteidx].price * temp[deleteidx].quantity);
+    setTotalAmount(newTotal);
+    temp.splice(deleteidx, 1);
+    setCart([...temp]);
+  }
+
+  const onCheckout = () => {
+    if (cart.length > 0) {
+      addDoc(collectionRefBills, {
+        customername: custName,
+        date: new Date(),
+        products: cart,
+        total: totalamount,
+      }).then(() => {
+        setBillDialog(true);
+      })
+    }
+    else {
+      alert("Cart Cannot be empty");
+      return;
+    }
+  }
+
+  const onDownload = () => {
+
+    const input = document.getElementById("bill");
+    html2canvas(input)
+      .then(canvas => {
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new jsPDF();
+        pdf.addImage(imgData, 'JPEG', 0, 0);
+        pdf.save(custName + date.toLocaleDateString() + ".pdf");
+      })
+
+  }
+
+  const onCloseBill = () => {
+
+    window.location.reload();
+
   }
 
   return (
-    <div className="row">
-      <div className="column left">
-        <DataGrid
-          checkboxSelection={true}
-          on
-          rows={inventory}
-          columns={[
-            {
-              field: 'id',
-              headerName: 'ID',
-              width: 100,
-            },
-            {
-              field: 'name',
-              headerName: 'Name',
-              sortable: true,
-              width: 100
-            },
-            {
-              field: 'description',
-              headerName: 'Description',
-              width: 100
-            },
-            {
-              field: 'price',
-              headerName: 'Price',
-              sortable: true,
-              width: 100
-            },
-            {
-              field: 'quantity',
-              headerName: 'Quantity',
-              sortable: true,
-              width: 100
-            }
-          ]}
-          pageSize={10}
+    <Container maxWidth="xl">
+      <Typography
+        variant="h3"
+        align="center"
+        gutterBottom
+      >
+        Billing
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} display="flex" alignItems="center" justifyContent="space-between">
+          <Typography
 
-        />
-      </div>
-      <div className="column right">
-        <div className="row up">
-      <TableContainer component={Paper}>
-                    <Table aria-label="simple table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Name</TableCell>
-                                <TableCell >Description</TableCell>
-                                <TableCell >Price</TableCell>
-                                <TableCell >Quantity</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {cart.map((item) => (
-                                <TableRow key={item.id} >
-                                    <TableCell>{item.name}</TableCell>
-                                    <TableCell>{item.description}</TableCell>
-                                    <TableCell>{item.price}</TableCell>
-                                    <TableCell>{item.quantity}</TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
+          >
+            Date: {date.toLocaleDateString()}
+          </Typography>
+          <Typography
+
+          >
+            Current Time:{curTime}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} display="flex" alignItems="center">
+          <Typography
+            margin="1%"
+          >
+            Customer Name:
+          </Typography>
+          <TextField
+            margin="normal"
+            sx={{
+              width: "40%"
+            }}
+            value={custName}
+            onChange={onChangeCustName}
+          >
+          </TextField>
+        </Grid>
+        <Grid item xs={12} display="flex" alignItems="center">
+          <Typography
+            margin="1%"
+          >
+            Product:
+          </Typography>
+          <Autocomplete
+            disablePortal
+            options={inventory}
+            value={curProduct}
+            onChange={handleCurProductChange}
+            inputValue={curProductInputValue}
+            isOptionEqualToValue={(option, value) => option.name === value.name}
+            onInputChange={handleCurProductInputValueChange}
+            getOptionLabel={(option) => option.name + " " + option.description + " "}
+            sx={{ width: "100%" }}
+            renderInput={(params) => <TextField {...params} label="Product" />}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          {curProduct.name != "" &&
+            <TableContainer component={Paper}>
+              <Table aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Name</TableCell>
+                    <TableCell >Price</TableCell>
+                    <TableCell >Quantity Available</TableCell>
+                    <TableCell >Description</TableCell>
+                    <TableCell >Category</TableCell>
+                    <TableCell >Tags</TableCell>
+                    <TableCell >Image</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow key={curProduct.id} >
+                    <TableCell>{curProduct.name}</TableCell>
+                    <TableCell>{curProduct.price}</TableCell>
+                    <TableCell>{curProduct.quantity}</TableCell>
+                    <TableCell>{curProduct.description}</TableCell>
+                    <TableCell>{curProduct.category.join(', ')}</TableCell>
+                    <TableCell>{curProduct.tag.join(', ')}</TableCell>
+                    <TableCell>
+                      <img src={curProduct.image} alt="product" width="100" height="100" />
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
+          }
+        </Grid>
+        <Grid item xs={12}>
+          {curProduct.name != "" &&
+            <div display="flex" alignitems="center">
+              <Typography
+                margin="1%"
+              >
+                Enter Quantity:
+              </Typography>
+              <TextField
+                error={curQuantityIsTouched && !curQuantityIsValid}
+                value={curQuantity}
+                onChange={onChangeCurQuantity}
+              >
+              </TextField>
+              {curQuantityIsTouched && !curQuantityIsValid && (
+                <div>
+                  <Typography color="red" variant="body1">
+                    {curQuantityErrMessage}
+                  </Typography>
+                  <Typography>
+                    Current Available Quantity: {curProduct.quantity}
+                  </Typography>
+                </div>
+              )}
+              <Button
+                variant="contained"
+                onClick={onAddToCart}
+              >
+                Add to Cart
+              </Button>
+            </div>
+          }
+        </Grid>
+        <Grid item xs={12}>
+          <div>
+            <Typography
+              variant="h5"
+            >
+              Cart
+              <ShoppingCartIcon />
+            </Typography>
+
+            {cart.length == 0 ?
+              <Typography
+                variant="h5"
+                align="center"
+              >
+                Cart is Empty
+              </Typography>
+              : <>
+                <TableContainer component={Paper} sx={{ marginBottom: "2%" }}>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Image</TableCell>
+                        <TableCell>Name</TableCell>
+                        <TableCell >Description</TableCell>
+                        <TableCell >Price</TableCell>
+                        <TableCell >Quantity</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {cart.map((item) => (
+                        <TableRow key={item.id} >
+                          <TableCell>
+                            <img src={item.image} alt={item.name} width="100" height="100" />
+                          </TableCell>
+                          <TableCell>{item.name}</TableCell>
+                          <TableCell>{item.description}</TableCell>
+                          <TableCell>{item.price}</TableCell>
+                          <TableCell>{item.quantity}</TableCell>
+                          <TableCell>
+                            <IconButton
+                              onClick={() => { onDeleteItem(item.id) }}
+                            >
+                              <DeleteIcon />
+                            </IconButton>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
                 </TableContainer>
-        </div>
-        <div className="row down">
-        <div>Information
-                              </div>
-        <Button variant="contained" color="primary">
-          Checkout
-        </Button>
-        </div> 
-      </div>
-    </div>
+              </>}
+            <div>
+              <Typography
+                gutterBottom
+              >
+                {totalamount != '' && <>Total amount: Rs.{totalamount}/-</>}
+              </Typography>
+            </div>
+            <div className="row down">
+              <Button variant="contained" color="primary" onClick={onCheckout}>
+                Checkout
+              </Button>
+            </div>
+          </div>
+        </Grid>
+      </Grid>
+      <Dialog
+        open={billDialog}
+        maxWidth="lg"
+      >
+        <Box
+          margin="2%"
+        >
+          <Box
+            id="bill"
+          >
+            <Typography
+              variant="h4"
+              align="center"
+            >
+              BILL
+            </Typography>
+            <Typography>
+              Date: {date.toLocaleDateString() + " " + curTime}
+            </Typography>
+            <Typography
+              align="left"
+            >
+              Customer Name: {custName}
+            </Typography>
+            <Typography
+            >
+              Products:
+            </Typography>
+            <TableContainer component={Paper} sx={{ marginBottom: "2%" }}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Image</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell >Description</TableCell>
+                    <TableCell >Price</TableCell>
+                    <TableCell >Quantity</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {cart.map((item) => (
+                    <TableRow key={item.id} >
+                      <TableCell>
+                        <img src={item.image} alt={item.name} width="100" height="100" />
+                      </TableCell>
+                      <TableCell>{item.name}</TableCell>
+                      <TableCell>{item.description}</TableCell>
+                      <TableCell>{item.price}</TableCell>
+                      <TableCell>{item.quantity}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <Typography
+              variant="h6"
+            >
+              Total Amount: Rs.{totalamount}/-
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+            >
+              Thank you for Shopping!
+            </Typography>
+          </Box>
+          <Box display="flex" justifyContent="space-between">
+            <Button
+              variant="contained"
+              onClick={onDownload}
+            >
+              Download Bill
+            </Button>
+            <Button
+              variant="contained"
+              onClick={onCloseBill}
+            >
+              Close
+            </Button>
+          </Box>
+        </Box>
+      </Dialog>
+    </Container>
   );
 }
 
